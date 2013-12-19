1dc6a1d24753293d8f1f53ad58bd6abae2895aca
App Academy Student <student@appacademy.io>
2013-12-18 18:04:42 -0800

    Routes

--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -31,16 +31,26 @@ class Router
   end
 
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
+  end
+
+  def get
+    @routes
   end
 
   def draw(&proc)
   end
 
   [:get, :post, :put, :delete].each do |http_method|
-    # add these helpers in a loop here
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
   end
 
   def run(req, res)
